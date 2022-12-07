b546128f4d5c2d4a417a5d30f700010b1d498df0
Martijn van Schie <martijnvanschie@gmail.com>
2022-12-07 20:47:47 +0100

    Added a check for required extension 'account'
    Fixes #2

---
+++ cli/Azure.Cli.Model/Extension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Azure.Cli.Model
+{ 
+    public class Extension
+    {
+        [JsonPropertyName("experimental")]
+        public bool Experimental { get; set; }
+
+        [JsonPropertyName("extensionType")]
+        public string ExtensionType { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("path")]
+        public string Path { get; set; }
+
+        [JsonPropertyName("preview")]
+        public bool Preview { get; set; }
+
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
+    }
+}
